53b55be4f954d3c5e77ae75ce40f5c554f062fd3
Protik Acharjay <protik7777777@gmail.com>
2024-11-24 18:31:29 +0600

    Model created and created migration and setup inside admin panel

--- proDjango/proApp/models.py
+++ proDjango/proApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ProItems(models.Model):
+    ITEM_TYPE_CHOICES = [
+        ('Car', 'Car'),
+        ('Product', 'Product'),
+        ('Docuement', 'Docuement'),
+        ('Other', 'Other'),
+    ]
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(null=True)
+    address = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='proImages/')
+    description = models.TextField(blank=True)
+    timezone = models.DateTimeField(default=timezone.now)
+    item_type = models.CharField(max_length=11,choices=ITEM_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
