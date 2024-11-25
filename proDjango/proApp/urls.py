6ecf77a0808cc6f3e0059c5161dd456ae794e97a
Protik Acharjay <82051598+ProtikAcharjay@users.noreply.github.com>
2024-11-25 10:42:00 +0600

    get route and model get_object_or_404 query

--- proDjango/proApp/urls.py
+++ proDjango/proApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.proAppFunction, name='proApp'),
+    path('data/', views.proData, name='proData'),
+    path('details/<int:item_id>', views.proDetail, name='item_detail'),
 ]
--- proDjango/proApp/views.py
+++ proDjango/proApp/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import ProItems
 # Create your views here.
 def proAppFunction(request):
     # return HttpResponse("Hello, World! This is the proApp function.")
     return render(request, 'proApp/pro.html')
+
+def proData(request):
+    pro_items = ProItems.objects.all()
+    return render(request, 'proApp/prodata.html', {'pro_items': pro_items})
+
+def proDetail(request, item_id):
+    pro_item = get_object_or_404(ProItems, id=item_id)
+    return render(request, 'proApp/prodetails.html', {'pro_item': pro_item})
